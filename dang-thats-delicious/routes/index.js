ea4b51444ce984a23d42a9c07760bccc27a58cbe
abrownhill <abrownhill@users.noreply.github.com>
2017-05-22 15:44:40 +0100

    Chore: More tidy up

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage);
+// router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
 
 router.get('/adb', (req, res) => {
   // res.send('Hey! It works!');
