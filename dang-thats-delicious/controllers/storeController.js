e44abf5b450e403ec5c1c09d738b492b4fdd9e53
abrownhill <abrownhill@users.noreply.github.com>
2017-05-22 15:40:32 +0100

    Comment out un-needed code ready for Video 9

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,10 +1,10 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = req.query.username || 'wesbos';
-  if (req.name === 'error') {
-    throw Error('I have triggered an error for you');
-  };
-  next();
-};
+// exports.myMiddleware = (req, res, next) => {
+//   req.name = req.query.username || 'wesbos';
+//   if (req.name === 'error') {
+//     throw Error('I have triggered an error for you');
+//   };
+//   next();
+// };
 
 exports.homePage = (req, res) => {
 console.log(req.name);
